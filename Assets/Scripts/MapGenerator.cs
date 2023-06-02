9d8bcbee9d7b79cbf79f613686958c157995f41d
mlystopad <mlystopadt@gmail.com>
2023-06-02 23:46:09 +0200

    Height Multiplier implemented

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -20,6 +20,9 @@ public class MapGenerator : MonoBehaviour
     public int seed;
     public Vector2 offset;
 
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
     public bool autoUpdate;
 
     public TerrainType[] regions;
@@ -56,7 +59,7 @@ public class MapGenerator : MonoBehaviour
         }
         else if (drawMode == DrawMode.Mesh)
         {
-            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap),
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve),
                 TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
         }
         
